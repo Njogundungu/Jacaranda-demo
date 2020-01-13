9fb96937e03e00fd0550d1a6e19a32c16264e90c
Njogundungu <njoguhenry57@gmail.com>
2020-01-13 15:03:00 +0300

    first git demo

---
+++ Demo.py
@@ -0,0 +1,10 @@
+Python 3.7.6 (tags/v3.7.6:43364a7ae0, Dec 19 2019, 01:54:44) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=3
+>>> b=4
+>>> c=a+b
+>>> c
+7
+>>> print("jacaranda demo")
+jacaranda demo
+>>> 
